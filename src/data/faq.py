a96269f23beb3dae100c388c89b825fd03ca83c2
Pablo Cavalcante <pablocavalcanteh@gmail.com>
2023-07-02 08:17:19 -0300

    Developing the conversation.

--- src/data/faq.py
+++ src/data/faq.py
@@ -1,4 +1,7 @@
 data = {
-    "Hi!": "Hello! I'm a generic chatbot. I'm here to help you. Tell me what you want...",
-    "I wish I could know how I can place an order through the website.": "Let's go! Firstly, go to the website address www.chatbot.com..."
+    "Hi!": "Hello! I'm a Chatbot. I'm here to help you. Tell me what you want...",
+    "I wish I could know how I can place an order through the website.": "Let's go! Firstly, go to the website address www.chatbot.com...",
+    "Ok! It's cool! Now, which is the next step?": "Now you can create an account!",
+    "Account created!": "Very well! Now, it's just access the page of the products!",
+    "Thank you so much for helping!": "You're welcome, Pablo Cavalcante! Bye!"
 }
